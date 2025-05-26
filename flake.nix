c41410a7a08562aa6be671f185d84dfeac48bea5
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-05-26 18:11:00 +0200

    ci: convco + update flake workflow

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
       pre-commit-check = lib.run {
         src = ./.;
         hooks = {
+          convco.enable = true;
           alejandra.enable = true;
           statix = {
             enable = true;
